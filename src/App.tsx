446f7b5e5c9444e506f545ac562fbca0f2054d48
Jing Qin <qin_jin@live.com>
2020-09-08 05:11:01 -0700

    add: Navigation Bar add: Blank pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import NavBar from './components/NavBar';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+
+      <NavBar/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
