4b49912ac649f9f1d1e35a7be5b42093dfce709f
Jing Qin <qin_jin@live.com>
2020-09-09 01:49:20 -0700

    Updates

---
+++ precache-manifest.1eaa41a65ce7c44deca798e836896901.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78acd9a28983110530e864f1f7c5fd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5950a112ba6da847c929",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "97a5cc1d0ce4948aa038",
+    "url": "/static/css/main.e26460a1.chunk.css"
+  },
+  {
+    "revision": "5950a112ba6da847c929",
+    "url": "/static/js/2.a955059d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a955059d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a5cc1d0ce4948aa038",
+    "url": "/static/js/main.493c64ef.chunk.js"
+  },
+  {
+    "revision": "bfe64756a92e059e4eca",
+    "url": "/static/js/runtime-main.486fdf55.js"
+  },
+  {
+    "revision": "e30c5ac159c82fa09bb710d0ffa345d4",
+    "url": "/static/media/bench-01.e30c5ac1.jpg"
+  },
+  {
+    "revision": "dac46edf74e0b7a83299fced776acb96",
+    "url": "/static/media/home-banner.dac46edf.jpg"
+  },
+  {
+    "revision": "9a4e2a7521d343fe54d2b3d2d1bae93a",
+    "url": "/static/media/logo-light.9a4e2a75.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1ca891d5df9db37f38c4d7ccbc62a228.js"
+  "/precache-manifest.1eaa41a65ce7c44deca798e836896901.js"
 );
 
 self.addEventListener('message', (event) => {
