d1f60aff604dc2180f0068645635041aa8070268
Jing Qin <qin_jin@live.com>
2020-09-09 15:48:26 -0700

    update: home banner font responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,13 +13,12 @@ function Home() {
             <div>
                 <div className="Home-banner" />
                 <div className="Home-banner-caption">
-                    <h1 className="Home-banner-caption-h1">Design, Manufacture, Polish, Deliver</h1>
-                    <h2 className="Home-banner-caption-h2">High-quality Stainless Steel Products</h2>
+                    <p className="Home-banner-caption-h1">Design, Manufacture, Polish, Deliver</p>
+                    <p className="Home-banner-caption-h2">High-quality Stainless Steel Products</p>
 
                     <p className="Home-banner-caption-btn"> 
                         <Button variant="outline-light" size="lg" block>Learn more</Button>
                     </p>
-                    
                 </div>
             </div>
             
