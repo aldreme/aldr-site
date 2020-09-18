726535285f945593b067cafa5de7d6ea1f4aafa3
ALDR <jin@aldreme.com>
2020-09-17 23:22:01 -0700

    add: ccp police license

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,8 @@ import './Footer.css';
 // the hook
 import { useTranslation } from 'react-i18next';
 
+import police_icon from '../assets/images/misc/ccp-police-icon.png';
+
 function Footer() {
   const { t } = useTranslation();
 
@@ -16,6 +18,12 @@ function Footer() {
         </div>
         <div className="Footer-icp">
           <a className="Footer-icp-license" href="http://beian.miit.gov.cn">{t("cn-icp-license")}</a>
+        </div>
+
+        <div className="Footer-ccp-police">
+          <a href="">
+            <img className="Footer-ccp-police-license-icon" alt="" src={police_icon} /> {t("cn-police-license")}
+          </a>
         </div>
       </Card.Footer>
     </Card>
